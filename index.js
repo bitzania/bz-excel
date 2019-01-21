480cb47da9f8da0c00919cfa093ad3fae363d6c9
xoriuz <ficky.irwanto@gmail.com>
2019-01-21 16:20:14 +0700

    - initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// Require library
+var xl = require('excel4node');
+
+module.exports = {
+    
+
+    createExcel: function(rows, fields, filename) {
+        // Create a new instance of a Workbook class
+        var wb = new xl.Workbook();
+
+        // Add Worksheets to the workbook
+        var ws = wb.addWorksheet('Sheet 1');
+
+        if (!fields) {
+            fields = Object.keys(rows[0]);
+        }
+
+        
+        // write fields
+        for (var j = 0; j< fields.length; j++) {
+            var field = fields[j];
+            ws.cell(1, j+1)["string"](field);
+        }
+
+        for (var i =0; i< rows.length; i ++) {
+            var row = rows[i];
+            for (var j = 0; j< fields.length; j++) {
+                var field = fields[j];
+
+                var value = row[field] || '';
+                // value = value.toString();
+                var type = typeof value;
+
+                switch (type) {
+                    case "boolean":
+                        type = "bool";
+                    break;
+
+                    case "object":
+                        if (Object.prototype.toString.call(value) === '[object Date]') {
+                            type = "date";
+                            console.log("ISDATE");
+                        }
+                        else {
+                            type = "string";
+                            value = value.toString();
+                        }
+                    break;
+
+                }
+                // console.log("type", type, "value", value, Object.prototype.toString.call(value)=== '[object Date]');
+                ws.cell(i+2, j+1)[type](value);
+            }
+
+        }
+
+        wb.write(filename);
+    }
+
+
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+
+var bz_excel = require('./index.js');
+var path = require("path");
+
+var data = [
+    {field1: 1, field2:"b", field3: true, field4: new Date()},
+    {field1: 2, field2:"c", field3: false, field4: new Date()},
+    {field1: 3, field2:"d", field3: true, field4: new Date()},
+    {field1: 4, field2:"e", field3: true, field4: new Date()},
+    {field1: 5, field2:"r", field3: null, field4: new Date()},
+    {field1: 6, field2:"g", field3: true, field4: new Date()},
+]
+
+console.log("WRITING EXCEL FILE : ", path.join(__dirname, "./xl.xlsx"));
+bz_excel.createExcel(data, ["field1", "field3", "field2", "field4"], path.join(__dirname, "./withField.xlsx"));
+bz_excel.createExcel(data, null, path.join(__dirname, "./default.xlsx"));
