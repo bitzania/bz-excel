7d4bb7a5b75ca2c250d8a942287737ffcd76d81f
xoriuz <ficky.irwanto@gmail.com>
2019-01-22 17:06:23 +0700

    - add field format

--- index.js
+++ index.js
@@ -12,29 +12,44 @@ module.exports = {
             // Add Worksheets to the workbook
             var ws = wb.addWorksheet('Sheet 1');
 
+            var types = [];
             if (!fields) {
                 fields = Object.keys(rows[0]);
             }
 
-            
+
             // write fields
             for (var j = 0; j< fields.length; j++) {
                 var field = fields[j];
+
+                console.log("FIELD", field);
+                if (field.indexOf(':')>-1) {
+                    var parts = field.split(':');
+                    field = parts[0].toString().trim();
+                    types.push(parts[1].toString().trim());
+                }
+                else types.push("auto");
                 ws.cell(1, j+1)["string"](field);
+                fields[j] = field;
             }
 
             for (var i =0; i< rows.length; i ++) {
                 var row = rows[i];
                 for (var j = 0; j< fields.length; j++) {
                     var field = fields[j];
-
+                    var otype = types[j];
                     var value = row[field] || '';
                     // value = value.toString();
                     var type = typeof value;
 
+                    if (otype != "auto") type = otype;
                     switch (type) {
                         case "boolean":
                             type = "bool";
+                        break;
+
+                        case "number": 
+                            value = value * 1;
                         break;
 
                         case "object":
@@ -49,7 +64,7 @@ module.exports = {
                         break;
 
                     }
-                    // console.log("type", type, "value", value, Object.prototype.toString.call(value)=== '[object Date]');
+                    console.log("type", type, "value", value, Object.prototype.toString.call(value)=== '[object Date]');
                     ws.cell(i+2, j+1)[type](value);
                 }
 
--- test.js
+++ test.js
@@ -3,20 +3,20 @@ var bz_excel = require('./index.js');
 var path = require("path");
 
 var data = [
-    {field1: 1, field2:"b", field3: true, field4: new Date()},
-    {field1: 2, field2:"c", field3: false, field4: new Date()},
-    {field1: 3, field2:"d", field3: true, field4: new Date()},
-    {field1: 4, field2:"e", field3: true, field4: new Date()},
-    {field1: 5, field2:"r", field3: null, field4: new Date()},
-    {field1: 6, field2:"g", field3: true, field4: new Date()},
+    {field1: 1, field2:"100.00", field3: true, field4: new Date()},
+    {field1: 2, field2:"11.00", field3: false, field4: new Date()},
+    {field1: 3, field2:"12.00", field3: true, field4: new Date()},
+    {field1: 4, field2:"13.00", field3: true, field4: new Date()},
+    {field1: 5, field2:"14.00", field3: null, field4: new Date()},
+    {field1: 6, field2:"15.00", field3: true, field4: new Date()},
 ]
 
 console.log("WRITING EXCEL FILE : ", path.join(__dirname, "./xl.xlsx"));
-bz_excel.createExcel(data, ["field1", "field3", "field2", "field4"], path.join(__dirname, "./withField.xlsx"))
+bz_excel.createExcel(data, ["field1", "field3", "field2:number", "field4"], path.join(__dirname, "./withField.xlsx"))
 .then((data)=> {
     console.log(data);
 })
 .catch((err)=> {
     console.log(err);
 })
-bz_excel.createExcel(data, null, path.join(__dirname, "./default.xlsx"));
+// bz_excel.createExcel(data, null, path.join(__dirname, "./default.xlsx"));
